420fb01b1a02b60ac95b5fc31b68a4b9e5c0ba61
krispaulbabu <krispaulbabu.krispaulbabu@students.mq.edu.au>
2023-03-28 01:52:48 +1100

    Worked on stage 1. Till I wanted to blow my brains out. Slightly better

--- Stage 1/client/MyClient.java
+++ Stage 1/client/MyClient.java
@@ -21,6 +21,9 @@ public class MyClient {
 			System.out.println(lastmessage);
 			dout.flush();
 
+			int times = 0;
+			String largestServer = "";
+
 			while (!(lastmessage.equals("NONE"))) {
 
 				dout.write(("REDY\n").getBytes());
@@ -36,35 +39,63 @@ public class MyClient {
 				String[] jobsIndivData = jobs.split(" ");
 				int jobId = Integer.parseInt(jobsIndivData[2]);
 
-				dout.write(("GETS All\n").getBytes());
-				String data = dis.readLine();
-				dout.flush();
+				int numberOfLargeServers = 0;
+				if (times < 1) {
 
-				dout.write(("OK\n").getBytes());
-				dout.flush();
+					dout.write(("GETS All\n").getBytes());
+					String data = dis.readLine();
+					dout.flush();
 
-				String[] indivData = data.split(" ");
-				int nRecs = Integer.parseInt(indivData[1]);
+					dout.write(("OK\n").getBytes());
+					dout.flush();
 
-				for (int i = 0; i < nRecs; i++) {
-					System.out.println((String) dis.readLine());
+					String[] indivData = data.split(" ");
+					int nRecs = Integer.parseInt(indivData[1]);
+
+					String servers = "";
+					String[][] serversList = new String[nRecs][];
+					int largestServerCores = 0;
+
+					System.out.println("We been here");
+					for (int i = 0; i < nRecs; i++) {
+						servers = (String) dis.readLine();
+						System.out.println(servers);
+
+						String[] serverIndiv = servers.split(" ");
+						serversList[i] = serverIndiv;
+
+						if (Integer.parseInt(serverIndiv[4]) > largestServerCores) {
+							largestServer = serverIndiv[0];
+							largestServerCores = Integer.parseInt(serverIndiv[4]);
+						}
+
+					}
+
+					System.out.println(largestServer);
+
+					for (int i = 0; i < nRecs; i++) {
+						if (serversList[i][0].equals(largestServer)) {
+							numberOfLargeServers += 1;
+						}
+					}
+					dout.write(("OK\n").getBytes());
+					dis.readLine();
+					dout.flush();
 				}
 
-				dout.write(("OK\n").getBytes());
-				dis.readLine();
-				dout.flush();
-
 				if (jobsIndivData[0].equals("JOBN")) {
-					String schedCommand = "SCHD " + jobId + " super-silk 0";
+					String schedCommand = "SCHD " + jobId + " " + largestServer + " 0";
 					System.out.println(schedCommand);
 					dout.write((schedCommand + "\n").getBytes());
-					System.out.println((String) dis.readLine() + "\n\n");
+					System.out.println((String) dis.readLine() + "\n");
 					dout.flush();
 				}
 
 				else {
 					System.out.println("\n\n");
 				}
+
+				times += 1;
 
 			}
 
